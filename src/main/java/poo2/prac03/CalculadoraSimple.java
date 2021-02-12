b97ed2c458790ea73ad64368e88b0aa6558bc001
Roberto Solis <rsolis@uaz.edu.mx>
2021-02-11 19:28:19 -0600

    Inicialización de Proyecto para Práctica 03

---
+++ src/main/java/poo2/prac03/CalculadoraSimple.java
@@ -0,0 +1,66 @@
+package poo2.prac03;
+
+/**
+ * Esta clase implementa cinco operaciones basicas para realizar operaciones
+ */
+public class CalculadoraSimple {
+    /**
+     * Este metodo devuelve la suma de dos numeros a y b
+     * @param a   Primer numero
+     * @param b   Segundo numero
+     * @return  a + b
+     */
+    public double suma(double a, double b) {
+        // TODO Elimina la siguiente linea y completa la implementacion
+        return 0;
+    }
+
+    /**
+     * Este metodo devuelve la resta de dos numeros a y b
+     * @param a   Primer numero
+     * @param b   Segundo numero
+     * @return  a - b
+     */
+    public double resta(double a, double b) {
+        // TODO Elimina la siguiente linea y completa la implementacion
+        return 0;
+    }
+
+    /**
+     * Este metodo devuelve el producto de dos numeros a y b
+     * @param a   Primer numero
+     * @param b   Segundo numero
+     * @return  a * b
+     */
+    public double mult(double a, double b) {
+        // TODO Elimina la siguiente linea y completa la implementacion
+        return 0;
+    }
+
+    /**
+     * Este metodo devuelve el cociente de la division de dos numeros a y b
+     * @param a   Primer numero
+     * @param b   Segundo numero
+     * @return  a / b
+     * @throws ArithmeticException  Si el numero b es cero indicando el mensaje "No se puede dividir entre cero"
+     */
+    public double divide(double a, double b)
+            throws ArithmeticException   {
+        // TODO Elimina la siguiente linea y completa la implementacion
+        return 0;
+    }
+
+    /**
+     * Este metodo eleva un numero a a la potencia b
+     * (NO ESTA PERMITIDO USAR FUNCIONES MATEMATICAS (paquete Math)
+     * PARA IMPLEMENTAR ESTE METODO)
+     * @param a  Valor double que indica la base de la potencia
+     * @param b  Valor entero que indica el exponente
+     * @return   a elevado a la b
+     */
+    public double eleva(double a, int b) {
+        // TODO Elimina la siguiente linea y completa la implementacion
+        return 0.0;
+    }
+
+}
